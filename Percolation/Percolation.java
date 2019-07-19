00c3dd89f74cf16664d3ecef4e928c4f7ab1c398
csabee005 <csabee005@gmail.com>
2019-07-19 15:04:01 +0200

    Basic imports, filled values now working on percolationtester

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -8,14 +8,24 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int size;
-    private WeightedQuickUnionUF[][] matrix;
+    private WeightedQuickUnionUF matrix;
+    private int[][] matrixValues;
+    private boolean[][] matrixOpenings;
 
     public Percolation(int size) {
         if (size <= 0) {
             throw new IllegalArgumentException("Size should be larger than 0. Current size is: "+ size);
         }
         this.size = size;
-        matrix = new WeightedQuickUnionUF[size][size];
+        matrix = new WeightedQuickUnionUF(size);
+        matrixValues = new int[size][size];
+        matrixOpenings = new boolean[size][size];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrixValues[i][j] = counter++;
+            }
+        }
     }
 
     public boolean isOpen(int row, int column) {
@@ -23,15 +33,38 @@ public class Percolation {
     }
 
     private boolean isMatrixPositionOpen(int row, int column) {
-       return matrix[row][column];
+        return matrixOpenings[row][column];
     }
 
     public void open(int row, int column) {
-        matrix[row-1][column-1] = true;
+        matrixOpenings[row][column] = true;
+        row -= 1;
+        column -= 1;
+        if (row + 1 < size && matrixOpenings[row + 1][column]) {
+            matrix.union(matrixValues[row][column], matrixValues[row + 1][column]);
+        }
+        if (row - 1 > 0 && matrixOpenings[row - 1][column]) {
+            matrix.union(matrixValues[row][column], matrixValues[row + 1][column]);
+        }
+        if (column + 1 < size && matrixOpenings[row][column + 1]) {
+            matrix.union(matrixValues[row][column], matrixValues[row][column + 1]);
+        }
+        if (column - 1 > 0 && matrixOpenings[row][column - 1]) {
+            matrix.union(matrixValues[row][column], matrixValues[row][column - 1]);
+        }
     }
 
     public boolean isFull(int row, int column) {
-        return matrix[row-1][column-1];
+        int columnNumber = 0;
+        row -= 1;
+        column -= 1;
+        while (columnNumber < size) {
+            if (columnNumber != column && matrix.connected(matrixValues[row][column], matrixValues[0][columnNumber])) {
+                return true;
+            }
+            columnNumber += 1;
+        }
+        return false;
     }
 
     public int getSize() {
